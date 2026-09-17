e1d604ebe5192e2096682487267c1d7334649da1
richard <rnegron@unsa.edu.pe>
2026-09-17 11:35:45 -0900

    avance del ejer 02

---
+++ lab03/ejercicio02.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+float partition(float arr[], int low, int high)
+{
+    float pivote = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivote)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+void quickSort(float arr[], float low, float high)
+{
+    if (low < high)
+    {
+        float pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+int main()
+{
+    float arr[] = {38, 27, 43, 3, 9, 82, 10};
+    float n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    cout << "Arreglo ordenado: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    return 0;
+}
